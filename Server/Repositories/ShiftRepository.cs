a6c13cd9bcc73f770d6c39e51db8dbd24ebe5af7
Orluff <nicolaj.orluff@gmail.com>
2023-05-12 12:27:23 +0200

    Fixed repo

--- Server/Repositories/ShiftRepository.cs
+++ Server/Repositories/ShiftRepository.cs
@@ -1,27 +1,97 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using Dapper;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using Npgsql;
 
 namespace Server.Repositories
 {
     public class ShiftRepository : IShiftRepository
     {
-        private const string connectionString = @"mongodb+srv://admin:Uyq39fea@shelter.poainkb.mongodb.net/test";
-        private const string databaseName = "shelterdb";
-        private const string bookingCollection = "bookings";
-        public IMongoCollection<ShiftDTO> shift_collection;
+        private const string connString = "UserID=dulxtoup;Password=14RylFQESpWlaG33iASfr1zUZgyh5JyS;Host=abul.db.elephantsql.com;Port=5432;Database=dulxtoup;";
 
         public ShiftRepository()
         {
-            var client = new MongoClient(connectionString);
-            var database = client.GetDatabase(databaseName);
-            shift_collection = database.GetCollection<ShiftDTO>(bookingCollection);
+            
         }
 
+        public ShiftDTO[] getShifts()
+        {
+            var result = new List<ShiftDTO>();
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM \"Shifts\"";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var ShiftID = reader.GetInt32(0);
+                        Console.WriteLine("Id=" + ShiftID);
+                        var StartDateTime = reader.GetDateTime(1);
+                        var EndDateTime = reader.GetDateTime(2);
+                        var Duration = reader.GetDouble(3);
+                        var Category_id = reader.GetInt32(4);
+                        var Priority = reader.GetBoolean(5);
+                        var Spots = reader.GetInt32(6);
+
+                        ShiftDTO b = new ShiftDTO { shift_id = ShiftID, startDateTime = StartDateTime, endDateTime = EndDateTime, duration = Duration, category_id = Category_id, priority = Priority, spots = Spots };
+                        result.Add(b);
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        public void AddShift(ShiftDTO shift)
+        {
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                /*
+                connection.Open();
+                var command = connection.CreateCommand();
+
+                command.CommandText = @"INSERT INTO Bike (Brand, Model, Description, Price, ImageUrl) VALUES ($brand, $model, $desc, $price, $imgurl)";
+                command.Parameters.AddWithValue("$brand", bike.Brand);
+                command.Parameters.AddWithValue("$model", bike.Model);
+                command.Parameters.AddWithValue("$desc", bike.Description);
+                command.Parameters.AddWithValue("$price", bike.Price);
+                command.Parameters.AddWithValue("$imgurl", bike.ImageUrl);
+                command.ExecuteNonQuery();
+                */
+            }
+        }
+
+        public void ReleaseShift(int shift_id)
+        {
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                
+            }
+        }
+
+        public void TakeShift(ShiftDTO shift)
+        {
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                
+            }
+        }
+
+        /*
         ShiftDTO[] IShiftRepository.getShifts()
         {
-            return shift_collection.Find(i => true).ToList().ToArray();
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                var users = connection.Query<UserDTO>(sql);
+
+                return sql;
+            }
         }
 
         void IShiftRepository.AddShift(ShiftDTO item)
@@ -39,6 +109,7 @@ namespace Server.Repositories
         {
             shift_collection.DeleteOne(item => item.shift_id == id);
         }
+        */
     }
 }
 
--- Shared/UserDTO.cs
+++ Shared/UserDTO.cs
@@ -11,24 +11,24 @@ public class UserDTO
     //Validering af fornavn
     [StringLength(40, ErrorMessage = "Navnet er for langt")]
     [MinLength(1, ErrorMessage = "Udfyld fornavn")]
-    public string name { get; set; } = "";
+    public string name { get; set; }
 
     //Validering af efternavn
     [StringLength(40, ErrorMessage = "Navnet er for langt")]
     [MinLength(1, ErrorMessage = "Udfyld efternavn")]
-    public string lastname { get; set; } = "";
+    public string lastName { get; set; }
 
-    public string address { get; set; } = "";
+    public string address { get; set; }
 
     //Validering af email
     [EmailAddress(ErrorMessage = "Indtast en valid email")]
-    public string email { get; set; } = "";
+    public string email { get; set; }
 
     //Validering af minimums længde på telefon nummer
     [StringLength(8, MinimumLength = 8, ErrorMessage = "Telefon nummer skal indehold mindst 8-cifre")]
-    public string telephone { get; set; } = "";
+    public string telephone { get; set; }
 
-    public DateTime birthDate { get; set; } = DateTime.Now.AddHours(+2);
+    public DateTime birthDate { get; set; }
 
     public int role_id { get; set; }
 }
