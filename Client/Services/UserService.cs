d6ffe1d0cae4d73bad5a84b64775e6c72985bff9
Orluff <nicolaj.orluff@gmail.com>
2023-05-16 10:34:26 +0200

    Mine oplysninger virker

--- Client/Services/UserService.cs
+++ Client/Services/UserService.cs
@@ -20,6 +20,12 @@ namespace Client.Services
             return users;
         }
 
+        public async Task<UserDTO> GetUserById(int userId)
+        {
+            var users = await getUsers();
+            return users.FirstOrDefault(u => u.user_id == userId);
+        }
+
         public async Task AddUser(UserDTO user)
         {
             await http.PostAsJsonAsync<UserDTO>("https://localhost:7201/api/user", user);
