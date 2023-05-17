f4835ba0587dcbe87b209e8988fabb00450fad24
Orluff <nicolaj.orluff@gmail.com>
2023-05-17 10:04:24 +0200

    Revert "Mine oplysninger virker"
    This reverts commit d6ffe1d0cae4d73bad5a84b64775e6c72985bff9.

--- Client/Services/IUserService.cs
+++ Client/Services/IUserService.cs
@@ -5,8 +5,7 @@ namespace Client.Services
 	public interface IUserService
 	{
 		Task<IEnumerable<UserDTO>> getUsers();
-        Task<UserDTO> GetUserById(int userId);
-        Task AddUser(UserDTO item);
+		Task AddUser(UserDTO item);
 		Task RemoveUser(UserDTO item);
 	}
 }
