5eb7ac6ec704454f909c0b0815c65b1b4866d03c
athi0017 <athitaya.petersen@gmail.com>
2023-05-18 12:58:34 +0200

    h

--- Shared/RoleDTO.cs
+++ Shared/RoleDTO.cs
@@ -5,7 +5,7 @@ public class RoleDTO
 {
     public int role_id { get; set; }
 
-    public string roleName { get; set; }
+    public string roleName { get; set; } = String.Empty;
 
     public string? description { get; set; }
 }
