41af55eb1e1a890ab8d39060d3b3bc3933f81974
Jakob Magni <jakobmagni@gmail.com>
2023-05-23 09:08:41 +0200

    Tilføjet AddSpot så det virker

--- Client/Services/ShiftService.cs
+++ Client/Services/ShiftService.cs
@@ -16,33 +16,33 @@ namespace Client.Services
 
         public async Task<IEnumerable<ShiftDTO>> getShifts()
         {
-            var shifts = await http.GetFromJsonAsync<ShiftDTO[]>(Config.serverURL+ "api/shift");
+            var shifts = await http.GetFromJsonAsync<ShiftDTO[]>("https://localhost:7201/api/shift");
             return shifts;
         }
 
         public async Task AddShift(ShiftDTO shift)
         {
-            await http.PostAsJsonAsync<ShiftDTO>(Config.serverURL + "api/shift", shift);
+            await http.PostAsJsonAsync("https://localhost:7201/api/shift/api/shift", shift);
         }
 
         public async Task RemoveShift(ShiftDTO shift)
         {
-            await http.DeleteAsync($"{Config.serverURL}api/shift/{shift.shift_id}");
+            await http.DeleteAsync($"https://localhost:7201/api/shift/{shift.shift_id}");
         }
 
         public async Task RemoveSpot(ShiftDTO shift)
         {
-            await http.PutAsJsonAsync($"{Config.serverURL}api/shift/remove/{shift.shift_id}", shift);
+            await http.PutAsJsonAsync($"https://localhost:7201/api/shift/remove/{shift.shift_id}", shift);
         }
 
         public async Task AddSpot(ShiftDTO shift)
         {
-            await http.PutAsJsonAsync($"{Config.serverURL}api/shift/add/{shift.shift_id}", shift);
+            await http.PutAsJsonAsync($"https://localhost:7201/api/shift/add/{shift.shift_id}", shift);
         }
 
         public async Task UpdateShift(ShiftDTO shift)
         {
-            await http.PutAsJsonAsync(Config.serverURL + "api/shift/", shift);
+            await http.PutAsJsonAsync("https://localhost:7201/api/shift", shift);
         }
     }
 }
