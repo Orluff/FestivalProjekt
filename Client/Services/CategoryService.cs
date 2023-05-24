813e87c9bb7dd3f63c6aca72ff567d1ab16debc6
helenakaad <helenakaad@hotmail.com>
2023-05-24 09:24:17 +0200

    små ændringer

--- Client/Services/CategoryService.cs
+++ Client/Services/CategoryService.cs
@@ -11,23 +11,22 @@ namespace Client.Services
     {
         HttpClient http;
 
-        // Konstruktøren for CategoryService-klassen.
-        // Den modtager en instans af HttpClient til at foretage HTTP-anmodninger.
+        // Modtager en instans af HttpClient til at foretage HTTP-anmodninger.
         public CategoryService(HttpClient http)
         {
             this.http = http;
         }
 
-        // Denne metode henter en liste af ShiftCategoryDTO-objekter fra serveren.
-        // Den foretager en asynkron HTTP GET-anmodning til serverens API-endepunkt.
+        // Henter en liste af ShiftCategoryDTO-objekter fra serveren.
+        // Laver en asynkron HTTP GET-anmodning til serverens API-endepunkt.
         public async Task<IEnumerable<ShiftCategoryDTO>> getCategories()
         {
             var cat = await http.GetFromJsonAsync<ShiftCategoryDTO[]>(Config.serverURL + "api/category");
             return cat;
         }
 
-        // Denne metode tilføjer en ny kategori til serveren.
-        // Den foretager en asynkron HTTP POST-anmodning til serverens API-endepunkt
+        // Tilføjer en ny kategori til serveren.
+        // Laver en asynkron HTTP POST-anmodning til serverens API-endepunkt
         // med ShiftCategoryDTO-objektet som payload.
         public async Task AddCategory(ShiftCategoryDTO cat)
         {
